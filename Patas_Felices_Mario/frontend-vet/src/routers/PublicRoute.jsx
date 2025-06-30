2472f064e7440521e0f178ff64ec4545123a2a99
MarioEndara <150484870+MarioEndara@users.noreply.github.com>
2025-06-30 09:02:19 -0500

    Delete Patas_Felices_Mario directory

--- Patas_Felices_Mario/frontend-vet/src/routers/PublicRoute.jsx
+++
@@ -1,9 +0,0 @@
-import { Navigate, Outlet } from "react-router"
-import StoreAuth from "../context/storeAuth"
-
-const PublicRoute =() =>{
-    const token = StoreAuth(state => state.token)
-    return token ? <Navigate to="/dashboard" /> : <Outlet />
-
-}
- export default PublicRoute
