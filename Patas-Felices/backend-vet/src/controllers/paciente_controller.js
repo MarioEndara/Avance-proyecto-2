648687c5d56cda2222b6e2697376ff1d904773d0
MarioEndara <150484870+MarioEndara@users.noreply.github.com>
2025-07-07 09:42:42 -0500

    Delete Patas-Felices directory

--- Patas-Felices/backend-vet/src/controllers/paciente_controller.js
+++
@@ -1,7 +0,0 @@
-const registrarPaciente = (req,res)=>{
-    res.send("Registro del paciente")
-
-}
-export{
-    registrarPaciente
-}
--- Patas-Felices/backend-vet/src/routers/paciente_routes.js
+++
@@ -1,8 +0,0 @@
-import {Router} from 'express'
-import { registrarPaciente } from '../controllers/paciente_controller.js'
-
-const router = Router()
-
-router.post('/paciente/registro',registrarPaciente)
-
-export default router
